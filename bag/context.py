5d5b5ccebaf674e7660f4d2150b0b837557e7094
Mya <myasenior13@outlook.com>
2021-03-28 14:41:48 +0000

    ATB

--- bag/context.py
+++ bag/context.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -7,23 +9,47 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
 
-    if total < settings.FREE_SHIPPING_THRESHOLD:
-        shipping = total * Decimal(settings.STANDARD_SHIPPING_PERCENTAGE / 100)
-        free_shipping_delta = settings.FREE_SHIPPING_THRESHOLD - total
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    
+                })
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
-        shipping = 0
-        free_shipping_delta = 0
+        delivery = 0
+        free_delivery_delta = 0
 
-    grand_total = shipping + total
+    grand_total = delivery + total
 
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
-        'shipping': shipping,
-        'free_shipping_delta': free_shipping_delta,
-        'free_shiping_threshold': settings.FREE_SHIPPING_THRESHOLD,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
+
     return context
