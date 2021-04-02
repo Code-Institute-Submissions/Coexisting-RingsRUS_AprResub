7226a4c137795915951d816985d47a9b0e82d6ae
Mya <myasenior13@outlook.com>
2021-04-02 16:52:00 +0000

    debug

--- RingsRUS/settings.py
+++ RingsRUS/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'home.urls')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['ringsrus.herokuapp.com', 'localhost']
 
